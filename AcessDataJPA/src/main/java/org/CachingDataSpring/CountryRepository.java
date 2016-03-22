15594693b99fe79b721edd4babe5e15d2cdf1096
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-03-22 20:23:29 +0000

    Enable caching

--- AcessDataJPA/src/main/java/org/CachingDataSpring/CountryRepository.java
+++ AcessDataJPA/src/main/java/org/CachingDataSpring/CountryRepository.java
@@ -2,10 +2,11 @@ package org.CachingDataSpring;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CountryRepository  extends CrudRepository<Country,Integer>{
-	
+public interface CountryRepository extends CrudRepository<Country, Integer> {
+	@Cacheable("countries")
 	List<Country> findByCurrency(String currency);
 
 }
